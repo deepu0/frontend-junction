720004e88cc649988d9d879cf5c57399c36456e4
Deepak Kumar <deepak.sharma@eminds.ai>
2024-03-18 14:47:18 +0530

    Refactor and clean code for form

--- form/components/main-content/MainContent.tsx
+++ form/components/main-content/MainContent.tsx
@@ -14,7 +14,7 @@ export function MainContent() {
 
   const { user } = useAuth();
   const { isLoading, isFailure, isSuccess } = useHandleKeypress();
-  useHandleScroll();
+  //useHandleScroll();
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
@@ -44,94 +44,33 @@ export function MainContent() {
     return <Success />;
   }
 
+  const questions = [
+    { index: 0, type: 'intro' },
+    { index: 1, type: 'firstName' },
+    { index: 2, type: 'lastName' },
+    { index: 3, type: 'industry' },
+    { index: 4, type: 'role' },
+    //{ index: 5, type: 'goal' },
+    { index: 5, type: 'description' },
+    { index: 6, type: 'email' },
+    { index: 7, type: 'identity' },
+  ];
+
   return (
     <section>
       <div>
-        <Question
-          type='intro'
-          outView={now - 1 === 0 || now > 1}
-          outViewSlide='up'
-          inView={now === 0}
-          inViewSlide={prev === 1 ? 'down' : ''}
-          isRendered={prev === null}
-        />
-
-        {[0, 2].includes(prev ?? -1) && [now - 1, now, now + 1].includes(1) && (
-          <Question
-            type='firstName'
-            outView={[now - 1, now + 1].includes(1)}
-            outViewSlide={now - 1 === 1 ? 'up' : 'down'}
-            inView={now === 1}
-            inViewSlide={prev === 2 ? 'down' : 'up'}
-          />
-        )}
-
-        {[1, 3].includes(prev ?? 0) && [now - 1, now, now + 1].includes(2) && (
-          <Question
-            type='lastName'
-            outView={[now - 1, now + 1].includes(2)}
-            outViewSlide={now - 1 === 2 ? 'up' : 'down'}
-            inView={now === 2}
-            inViewSlide={prev === 3 ? 'down' : 'up'}
-          />
-        )}
-
-        {[2, 4].includes(prev ?? 0) && [now - 1, now, now + 1].includes(3) && (
-          <Question
-            type='industry'
-            outView={[now - 1, now + 1].includes(3)}
-            outViewSlide={now - 1 === 3 ? 'up' : 'down'}
-            inView={now === 3}
-            inViewSlide={prev === 4 ? 'down' : 'up'}
-          />
-        )}
-
-        {[3, 5].includes(prev ?? 0) && [now - 1, now, now + 1].includes(4) && (
-          <Question
-            type='role'
-            outView={[now - 1, now + 1].includes(4)}
-            outViewSlide={now - 1 === 4 ? 'up' : 'down'}
-            inView={now === 4}
-            inViewSlide={prev === 5 ? 'down' : 'up'}
-          />
-        )}
-
-        {[4, 6].includes(prev ?? 0) && [now - 1, now, now + 1].includes(5) && (
-          <Question
-            type='goal'
-            outView={[now - 1, now + 1].includes(5)}
-            outViewSlide={now - 1 === 5 ? 'up' : 'down'}
-            inView={now === 5}
-            inViewSlide={prev === 6 ? 'down' : 'up'}
-          />
-        )}
-        {[5, 7].includes(prev ?? 0) && [now - 1, now, now + 1].includes(6) && (
-          <Question
-            type='description'
-            outView={[now - 1, now + 1].includes(6)}
-            outViewSlide={now - 1 === 6 ? 'up' : 'down'}
-            inView={now === 6}
-            inViewSlide={prev === 7 ? 'down' : 'up'}
-          />
-        )}
-        {[6, 8].includes(prev ?? 0) && [now - 1, now, now + 1].includes(7) && (
-          <Question
-            type='email'
-            outView={[now - 1, now + 1].includes(7)}
-            outViewSlide={now - 1 === 7 ? 'up' : 'down'}
-            inView={now === 7}
-            inViewSlide={prev === 8 ? 'down' : 'up'}
-          />
-        )}
-
-        {prev === 7 && [now - 1, now, now + 1].includes(8) && (
-          <Question
-            type='identity'
-            outView={[now - 1, now + 1].includes(8)}
-            outViewSlide={now - 1 === 8 ? 'up' : 'down'}
-            inView={now === 8}
-            inViewSlide={'up'}
-          />
+        {questions.map(
+          ({ index, type }) =>
+            [prev, now].includes(index) && (
+              <Question
+                key={index}
+                type={type as any}
+                outView={now - 1 === index}
+                outViewSlide={now - 1 === index ? 'up' : 'down'}
+                inView={now === index}
+                inViewSlide={prev === index ? 'down' : 'up'}
+              />
+            )
         )}
       </div>
     </section>
--- form/hooks/useHandleKeypress.ts
+++ form/hooks/useHandleKeypress.ts
@@ -6,7 +6,11 @@ import {
 } from '@/form/utils';
 import useCustomSubmit from './useSubmit';
 import { useEffect } from 'react';
-
+interface ValidationRule {
+  questionType: string;
+  errorMessage: string;
+  checkCondition: () => boolean;
+}
 export function useHandleKeypress() {
   const { questionNum, setErrorMsg, handleQuestionNumUpdate } =
     useSharedStates();
@@ -25,87 +29,139 @@ export function useHandleKeypress() {
     identity,
   } = state;
 
+  const validationRules: ValidationRule[] = [
+    {
+      questionType: 'firstName',
+      errorMessage: 'Please fill this in',
+      checkCondition: () => state.firstName === '',
+    },
+    {
+      questionType: 'lastName',
+      errorMessage: 'Please fill this in',
+      checkCondition: () => state.lastName === '',
+    },
+    {
+      questionType: 'industry',
+      errorMessage: 'Oops! Please make a selection',
+      checkCondition: () => state.industry === '',
+    },
+    {
+      questionType: 'role',
+      errorMessage: 'Oops! Please make a selection',
+      checkCondition: () => state.role === '',
+    },
+    {
+      questionType: 'goal',
+      errorMessage: 'Oops! Please make a selection',
+      checkCondition: () => state.goals.length === 0,
+    },
+    {
+      questionType: 'goal',
+      errorMessage: 'Please select more choices',
+      checkCondition: () => state.goals.length === 1,
+    },
+    {
+      questionType: 'description',
+      errorMessage: 'Please fill this in',
+      checkCondition: () => state.description === '',
+    },
+    {
+      questionType: 'email',
+      errorMessage: 'Please fill this in',
+      checkCondition: () => state.email === '',
+    },
+    {
+      questionType: 'email',
+      errorMessage: "Hmm... that link doesn't look right",
+      checkCondition: () => !isValidLinkedInUrl(state.email),
+    },
+    {
+      questionType: 'identity',
+      errorMessage: 'Oops! Please make a selection',
+      checkCondition: () => state.identity === '',
+    },
+  ];
+
   useEffect(() => {
     function handleKeypress(event: KeyboardEvent) {
       if (event.key === 'Enter') {
         event.preventDefault();
+        const currentQuestionType = getCurrentQuestionType(now);
+        const rule = validationRules.find(
+          (rule) => rule.questionType === currentQuestionType
+        );
 
-        if (now + 1 === 2 && firstName === '') {
+        if (rule && rule.checkCondition()) {
           setErrorMsg((prevValue) => ({
             ...prevValue,
-            firstName: 'Please fill this in',
-          }));
-          return;
-        } else if (now + 1 === 3 && lastName === '') {
-          setErrorMsg((prevValue) => ({
-            ...prevValue,
-            lastName: 'Please fill this in',
-          }));
-          return;
-        } else if (now + 1 === 4 && industry === '') {
-          setErrorMsg((prevValue) => ({
-            ...prevValue,
-            industry: 'Oops! Please make a selection',
-          }));
-          return;
-        } else if (now + 1 === 5 && role === '') {
-          setErrorMsg((prevValue) => ({
-            ...prevValue,
-            role: 'Oops! Please make a selection',
-          }));
-          return;
-        } else if (now + 1 === 6 && goals.length === 0) {
-          setErrorMsg((prevValue) => ({
-            ...prevValue,
-            goals: 'Oops! Please make a selection',
-          }));
-          return;
-        } else if (now + 1 === 6 && goals.length === 1) {
-          setErrorMsg((prevValue) => ({
-            ...prevValue,
-            goals: 'Please select more choices',
-          }));
-          return;
-        } else if (now + 1 === 7 && description === '') {
-          setErrorMsg((prevValue) => ({
-            ...prevValue,
-            email: 'Please fill this in',
-          }));
-          return;
-        } else if (now + 1 === 8 && email === '') {
-          setErrorMsg((prevValue) => ({
-            ...prevValue,
-            email: 'Please fill this in',
-          }));
-          return;
-        } else if (now + 1 === 8 && email && !isValidLinkedInUrl(email)) {
-          setErrorMsg((prevValue) => ({
-            ...prevValue,
-            email: "Hmm... that link doesn't look right",
-          }));
-          return;
-        } else if (now + 1 === 9 && identity === '') {
-          setErrorMsg((prevValue) => ({
-            ...prevValue,
-            role: 'Oops! Please make a selection',
+            [currentQuestionType]: rule.errorMessage,
           }));
           return;
         }
-        // } else if (
-        //   now + 1 === 8 &&
-        //   email &&
-        //   !isNotValidEmail(email) &&
-        //   isTaskSpecificEmail(email)
-        // ) {
+        // if (now + 1 === 2 && firstName === '') {
         //   setErrorMsg((prevValue) => ({
         //     ...prevValue,
-        //     email: 'Hmm... task specific emails are not allowed',
+        //     firstName: 'Please fill this in',
+        //   }));
+        //   return;
+        // } else if (now + 1 === 3 && lastName === '') {
+        //   setErrorMsg((prevValue) => ({
+        //     ...prevValue,
+        //     lastName: 'Please fill this in',
+        //   }));
+        //   return;
+        // } else if (now + 1 === 4 && industry === '') {
+        //   setErrorMsg((prevValue) => ({
+        //     ...prevValue,
+        //     industry: 'Oops! Please make a selection',
+        //   }));
+        //   return;
+        // } else if (now + 1 === 5 && role === '') {
+        //   setErrorMsg((prevValue) => ({
+        //     ...prevValue,
+        //     role: 'Oops! Please make a selection',
+        //   }));
+        //   return;
+        // } else if (now + 1 === 6 && goals.length === 0) {
+        //   setErrorMsg((prevValue) => ({
+        //     ...prevValue,
+        //     goals: 'Oops! Please make a selection',
+        //   }));
+        //   return;
+        // } else if (now + 1 === 6 && goals.length === 1) {
+        //   setErrorMsg((prevValue) => ({
+        //     ...prevValue,
+        //     goals: 'Please select more choices',
+        //   }));
+        //   return;
+        // } else if (now + 1 === 7 && description === '') {
+        //   setErrorMsg((prevValue) => ({
+        //     ...prevValue,
+        //     email: 'Please fill this in',
+        //   }));
+        //   return;
+        // } else if (now + 1 === 8 && email === '') {
+        //   setErrorMsg((prevValue) => ({
+        //     ...prevValue,
+        //     email: 'Please fill this in',
+        //   }));
+        //   return;
+        // } else if (now + 1 === 8 && email && !isValidLinkedInUrl(email)) {
+        //   setErrorMsg((prevValue) => ({
+        //     ...prevValue,
+        //     email: "Hmm... that link doesn't look right",
+        //   }));
+        //   return;
+        // } else if (now + 1 === 9 && identity === '') {
+        //   setErrorMsg((prevValue) => ({
+        //     ...prevValue,
+        //     role: 'Oops! Please make a selection',
         //   }));
         //   return;
         // }
 
         handleQuestionNumUpdate();
-        if (now === 8) {
+        if (now === 7) {
           onSubmit();
         }
       }
@@ -135,4 +191,27 @@ export function useHandleKeypress() {
     isFailure,
     isSuccess,
   };
+}
+
+function getCurrentQuestionType(now: number): string {
+  switch (now) {
+    case 1:
+      return 'firstName';
+    case 2:
+      return 'lastName';
+    case 3:
+      return 'industry';
+    case 4:
+      return 'role';
+    // case 5:
+    //   return 'goal';
+    case 5:
+      return 'description';
+    case 6:
+      return 'email';
+    case 7:
+      return 'identity';
+    default:
+      return '';
+  }
 }
